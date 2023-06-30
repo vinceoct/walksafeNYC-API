1e4e51d18c3302c7a2c048f798110e963c2743ae
LowellC86 <lowellcaraballo@gmail.com>
2023-06-30 15:44:10 -0400

    adjusting time

--- Models/Post.js
+++ Models/Post.js
@@ -4,6 +4,7 @@ const postSchema = new Schema (
 {
     post_id:{type: Number, required: true},
     post_date: {type: String, required: true}, 
+    post_time:{String, required: true},
     user_account:{type: String, required: true},
     status:{ type: Boolean, required: true},
     post_type:{ type: String, required: true},
