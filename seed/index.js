fad9526b42daf41f9f78f88355662fb4b7f54f33
LowellC86 <lowellcaraballo@gmail.com>
2023-06-30 14:45:28 -0400

    adjusting seed

--- seed/index.js
+++ seed/index.js
@@ -1,51 +1,47 @@
-const db = require('../db')
-const { Admin, Post, UserAccount } = require('../models')
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-console.log("index.js in seed is running")
+const db = require('../db');
+const { Admin, Post, UserAccount } = require('../models');
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+console.log("index.js in seed is running");
 
 db.once('open', async () => {
-  console.log('Connected to MongoDB')
+  console.log('Connected to MongoDB');
 
   const main = async () => {
-    
-    const Admins = [
+    const admins = [
       {
         admin_id: 'Vince Vullo',
         userName: 'winceton',
         password: 'helloWorld1'
       },
-
       {
         admin_id: 'Margaret Camilletti',
         userName: 'margaretcamiletti',
         password: 'helloWorld2'
       },
-
       {
         admin_id: 'Daniel Mulka',
         userName: 'danmulka',
         password: 'helloWorld3'
       },
-
       {
         admin_id: 'Lowell Caraballo',
         userName: 'lowellC86',
         password: 'helloWorld4'
       }
-    ]
+    ];
 
-    const Posts = [
+    const posts = [
       {
-        post_id:0,
+        post_id: 0,
         post_date: "2023-06-30-0100",
         user_account: 1,
-        status: Active,
+        status: true,
         post_type: "Harassment",
-        post_reply: "Your post has been submitted and under review. Thank you for your patience. We will contact you within 10-20 minutes"
+        post_reply: "Your post has been submitted and is under review. Thank you for your patience. We will contact you within 10-20 minutes."
+      }
+    ];
 
-      }]
-    
-    const Users = [
+    const userAccounts = [
       {
         user_account: "AnneBelleJ00",
         first_name: "Annebell",
@@ -54,25 +50,16 @@ db.once('open', async () => {
         password: "wonderWoman2001!",
         date_of_birth: "1986-02-23",
         gender: "Female"
+      }
+    ];
 
-      }]
+    await Admin.insertMany(admins);
+    console.log("Admins inserted successfully!");
+    await Post.insertMany(posts);
+    console.log("Posts inserted successfully!");
+    await UserAccount.insertMany(userAccounts);
+    console.log("User accounts inserted successfully!");
+  };
 
-      await Admin.insertMany(phones)
-      console.log("Check these phones out!")
-      await Post.insertMany(Platforms)
-      console.log('platform created')
-      await UserAccount.insertMany(Makes)
-      console.log('make created')
-}
-
-    
-const run = async () => {
-  console.log("running")
-  await main()
-  db.close()   
-} 
-
-run()
-})
-
-  
+  await main();
+});
