07768730b314902116eefe1507c3e473b84e022d
LowellC86 <lowellcaraballo@gmail.com>
2023-07-01 13:31:47 -0400

    seeded files

--- Models/UserAccount.js
+++ Models/UserAccount.js
@@ -2,7 +2,7 @@ const { Schema } = require('mongoose')
 
 const userAccountSchema = new Schema (
 {
-    user_account: {type: Number, required: true},
+    
     first_name:{type: String, required: true}, 
     last_name:{type: String, required: true},
     email:{type: String, required: true},
--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/")
+  .connect("mongodb://127.0.0.1:27017/walksafeDB")
   .then(() => {
     console.log("Successfully connected to mongoDB");
   })
