3fbb0d69fad3d25901de6ed68a103c7bb3243d15
LowellC86 <lowellcaraballo@gmail.com>
2023-07-03 11:14:44 -0400

    adminroute

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,6 +3,8 @@ const controller = require('../controllers/adminController.js')
 
 Router.get('/', controller.getAdmins)
 Router.get('/:id', controller.getAdminById)
+Router.put('/', controller.createAdmin)
+Router.delete('/:id', controller.deleteAdmin)
 
 module.exports = Router;
 
