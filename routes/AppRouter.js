f355b4e95c13ddab0e5e10efd24d811d6d85dc99
Vincent Vullo <vincevullo@gmail.com>
2023-07-03 10:03:14 -0400

    setup approuter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,9 +1,13 @@
 const Router = require(`express`).Router();
 
-const UserRouter = require('./userRouter')
+const postRouter = require('./postRouter')
+const userRouter = require('./userRouter')
+const adminRouter = require('./adminRouter')
 
-Router.use('/users', UserRouter)
-
+Router.use('/posts', postRouter)
+Router.use('/users', userRouter)
+Router.use('/admin', adminRouter)
+Router.get("/", (req, res) => res.send('in NEW YOOOOOOOOOORK!'));
 
 
 module.exports = Router;
